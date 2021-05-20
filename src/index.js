e95c8338d827866d402801006cfcc4396edac967
Alan Escobedo <alaan1132@gmail.com>
2021-05-19 22:02:55 -0500

    ADD: Auth0-example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider 
+      domain='alanescobedo.us.auth0.com' clientId='IAPssILUlATqleJdMcpwNrSizt2IxtEo' 
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
